2261545945c5c1cd22509dc308c449c92f5b88ae
IvanCCO <ivan.miranda@sptech.school>
2022-06-17 16:38:38 -0300

    Nâo consigo comparar Strings ;(

--- aula01/Caneta.java
+++ aula01/Caneta.java
@@ -1,56 +1,78 @@
-  
-  package aula01;
+import java.util.Scanner;
 
-class Caneta{
+class Caneta {
 
-        String modelo;
-        String cor;
-        float ponta;
-        int carga;
-        boolean tampada;
-        void status(){
+  String modelo;
+  String cor;
+  float ponta;
+  int carga;
+  boolean tampada;
 
-          System.out.println("Uma caneta " + this.cor);
-          System.out.println("Esta tampada?: " + this.tampada);
-        }
-        void rabiscar(){
+  void status() {
 
-          if(this.tampada == true){
+    System.out.println("Uma caneta " + this.cor);
+    System.out.println("Esta tampada?: " + this.tampada);
+  }
 
-            System.out.println("Você pode Rabiscar");
+  void rabiscar() {
 
-          }else{
+    if (this.tampada == true) {
 
-            System.out.println("Você não pode Rabiscar");
-          }
+      System.out.println("Você pode Rabiscar");
 
-          
+    } else {
 
-        }
-
-        void tampar(){
-
-          String array_cor[] =
-          {"Verde","Preto","Roxo","Azul-Claro","Marrom"};
-        
-          System.out.println(this.modelo.length());
-
-            for(int i = 0; i < array_cor.length ; i++){
-
-              this.cor = array_cor[i];
-
-              System.out.println("A caneta está na cor: " + this.cor);
-
-              if(this.cor == "Marrom"){
-
-                System.out.println("Deu certo Uhull!!");
-              }
-        
-          }
-        }
-
-        void destampar(){
-
-
-        }
+      System.out.println("Você não pode Rabiscar");
     }
+
+  }
+
+  void tampar() {
+
+    String array_cor[] = { "Verde", "Preto", "Roxo", "Azul-Claro", "Marrom" };
+
+    System.out.println(this.modelo.length());
+
+    for (int i = 0; i < array_cor.length; i++) {
+
+      this.cor = array_cor[i];
+
+      if (this.cor == "Preto") {
+
+        try (Scanner ColorObj = new Scanner(System.in)) {
+          int volta = 1;
+
+          String array_resposta[] = {
+              "y", "Y", "n", "N"
+          };
+
+          for (int j = 0; j < volta; j++) {
+
+            System.out.println("Você gosta da cor Preto? \n Digite Y ou N");
+
+            String likedColor = ColorObj.nextLine();
+
+            System.out.println("likedColor é " + likedColor);
+
+            for (int k = 0; k < array_resposta.length; k++) {
+
+              if(likedColor == array_resposta[k]){
+
+                System.out.println("Entrou no Certo");
+              }else if(likedColor.isEmpty()){
+
+                System.out.println("Digite valores validos");
+                System.out.println(j); 
+              }else{
+
+                System.out.println("SCORORR");
+              }
+            }
+
+          }
+        }
+
+      }
+    }
+  }
+}
